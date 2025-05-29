5715b640390254972bc3184033dbcf6f543b3471
Shovik Banerjee <shovikbanerjee@Shoviks-MacBook-Air.local>
2025-05-29 16:54:46 +0530

    updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Required for Render to know where to serve the files from
+  },
+  server: {
+    port: 5173, // Optional: Useful for local dev
+  },
 });
